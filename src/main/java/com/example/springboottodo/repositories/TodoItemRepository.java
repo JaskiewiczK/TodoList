c24a5668ac172da61f0afe62a6f8247d5aeb726d
JaskiewiczK <116189743+JaskiewiczK@users.noreply.github.com>
2023-10-22 14:50:36 +0200

    project commit

---
+++ src/main/java/com/example/springboottodo/repositories/TodoItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.springboottodo.repositories;
+
+import com.example.springboottodo.models.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+
+}
